cb7567f9a6c65e7dec95440cfe8721c111cfae1d
Devin Pastoor <devin.pastoor@gmail.com>
2015-02-02 09:55:31 -0500

    add so can set multiple sets of parameters

--- base_added_r.R
+++ base_added_r.R
@@ -3,21 +3,38 @@ library(ggplot2)
 Paw <- list(name='Paw',time=seq(from=0, to=40, by=0.1))
 KIN <- list(name='kin',time=seq(from=0, to=40, by=0.1))
 
-p <- c(kgrow = 0.718,
-       kin0 = 26,
+
+pdf <- data.frame(id = 1,
+                  kgrow = 0.718,
+                  kin0 = 26,
+                  Imax = 0.289,
+                  Cp = 0,
+                  IC50 = 22.7,
+                  kout = 0.137,
+                  tonset = 14.5,
+                  Rdeg = 0.1,
+                  Paw0 = 64.8)
+
+p <- c(id = 2,
+  kgrow = 0.718,
+       kin0 = 12,
        Imax = 0.289,
        Cp = 0,
        IC50 = 22.7,
        kout = 0.137,
        tonset = 14.5,
-       Rdeg = 0.1,
+       Rdeg = 0.128,
        Paw0 = 64.8)
 
+param <- rbind(pdf, p)
+
 res <- simulx(model='base_added.txt', 
-              parameter=p, 
+              parameter=param, 
               output=list(Paw, KIN))
 
-ggplot(data=res$Paw, aes(x=time, y=Paw)) + geom_line(size=1) +
+ggplot(data=res$Paw, aes(x=time, y=Paw, color = id)) + geom_line(size=1) +
   PKPDmisc::base_theme() + scale_y_continuous(breaks = c(seq(40, 120, 10)))
 
 print(ggplot(data=res$kin, aes(x=time, y=kin)) + geom_line(size=1))
+
+res
